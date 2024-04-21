b75061ea6fb946bff1bce8b1173a5cb113a3b4ab
Akshayraj5353 <158462617+Akshayraj5353@users.noreply.github.com>
2024-04-22 04:05:20 +0530

    Update Navbar.js to test jenkins clone repo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
           <div className="container-fluid">
-            <Link className="navbar-brand fs-3 fst-italic" to="/">CaterOrange</Link>
+            <Link className="navbar-brand fs-3 fst-italic" to="/">CaterOrange-V1</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
